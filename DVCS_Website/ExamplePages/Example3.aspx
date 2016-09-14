a14cae368b87b24c3922eeb7f8a35561a1785168
ccebreros1 <cesar.cebreros10@gmail.com>
2016-09-13 20:03:37 -0600

    Adds a third webpage that has a button with simple code to change its background

---
+++ DVCS_Website/ExamplePages/Example3.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Example3.aspx.cs" Inherits="ExamplePages_Example3" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
+    <h1>This is a third webpage, this one adds a button that changes its background to yellow or White</h1>
+    <asp:Button ID="ChangeColor" runat="server" Text="Change to yellow" BackColor="#666666" ForeColor="Black" OnClick="ChangeColor_Click" />
+</asp:Content>
+
---
+++ DVCS_Website/ExamplePages/Example3.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ExamplePages_Example3 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+
+    protected void ChangeColor_Click(object sender, EventArgs e)
+    {
+        if (ChangeColor.Text=="Change to yellow")
+        {
+            ChangeColor.BackColor = System.Drawing.Color.Yellow;
+            ChangeColor.Text = "Change to default";
+        }
+        else
+        {
+            ChangeColor.BackColor = System.Drawing.Color.SlateGray;
+            ChangeColor.Text = "Change to yellow";
+        }
+        
+    }
+}
